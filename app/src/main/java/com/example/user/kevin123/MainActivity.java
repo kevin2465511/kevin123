e705a951929855998a179baa5ede808c3f875e0d
kevin2465511 <kevin2465511@gmail.com>
2016-08-23 17:02:22 +0800

    finish MainActivity's UI and add button clicked logic

--- app/src/main/java/com/example/user/kevin123/MainActivity.java
+++ app/src/main/java/com/example/user/kevin123/MainActivity.java
@@ -3,14 +3,41 @@ package com.example.user.kevin123;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView infoText;
+    EditText nameEditText;
+    RadioGroup optionsGroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("test", "some logs");
-        Lod.d("test", "test");
+        setContentView(R.layout.activity_new_main);
+
+        //find UId by their ids
+        infoText = (TextView)findViewById(R.id.infoText);
+        nameEditText = (EditText)findViewById(R.id.nameEditText);
+        optionsGroup = (RadioGroup)findViewById(R.id.optionsGroup);
+        Button confirmBtn = (Button)findViewById(R.id.confirmButton);
+
+        confirmBtn.setOnClickListener(MainActivity.this);
+
+    public void onClick(View v){
+        int viewId = v.getId();
+        if(viewId == R.id.confirmButton){
+            //confirmButton was clicked
+            Log.d("buttonTest","confirmButton was clicked");
+
+        }
+    }
+
+
     }
 }
