3f6b2ce2e9eb42df21a47f53cd6155ad75869c78
kevin2465511 <kevin2465511@gmail.com>
2016-08-23 14:41:22 +0800

    add Log to MainActivity

--- app/src/main/java/com/example/user/kevin123/MainActivity.java
+++ app/src/main/java/com/example/user/kevin123/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test", "some logs");
+        Lod.d("test", "test");
     }
 }
